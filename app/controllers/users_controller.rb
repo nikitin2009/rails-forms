97c1ef0e8f017d40d76e923ea84679efe9ae7d34
Alex <nikitin2009@gmail.com>
2019-03-20 12:23:15 +0200

    Implement User#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    # user = params[:user]
-    username = params[:username]
-    email = params[:email]
-    password = params[:password]
-    puts "#####"
-    puts "#{username} #{email} #{password}"
-    puts "#####"
+    @user = User.new(username: params[:username],
+                        email: params[:email], 
+                     password: params[:password])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
