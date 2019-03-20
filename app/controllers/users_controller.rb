2c394764fca18051329153eb70abbfdb7e91478a
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-03-20 13:01:06 +0300

    add form to new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    # user = params[:user]
+    username = params[:username]
+    email = params[:email]
+    password = params[:password]
+    puts "#####"
+    puts "#{username} #{email} #{password}"
+    puts "#####"
   end
 end
